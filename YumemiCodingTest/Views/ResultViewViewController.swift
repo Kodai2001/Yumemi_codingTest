501e6497a85d8476d630f6555a8f97076051a46d
Kodai <system@Kodai-HayashinoMacBook.local>
2021-10-25 19:08:18 +0900

    UI of ResultVC has finished

---
+++ YumemiCodingTest/Views/ResultViewViewController.swift
@@ -0,0 +1,146 @@
+//
+//  ResultViewViewController.swift
+//  YumemiCodingTest
+//
+//  Created by system on 2021/10/25.
+//
+
+import UIKit
+
+class ResultViewViewController: UIViewController {
+    
+    let vc = ViewController()
+    
+    let imageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.backgroundColor = .red
+        
+        return imageView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Title"
+        label.tintColor = .secondaryLabel
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 30, weight: .semibold)
+        return label
+    }()
+    
+    let languageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "languageLabel"
+        label.tintColor = .secondaryLabel
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 20, weight: .semibold)
+        return label
+    }()
+    
+    let starsLabel: UILabel = {
+        let label = UILabel()
+        label.text = "starsLabel"
+        label.tintColor = .secondaryLabel
+        label.textAlignment = .right
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        return label
+    }()
+    
+    let watchersLabel: UILabel = {
+        let label = UILabel()
+        label.text = "watchersLabel"
+        label.tintColor = .secondaryLabel
+        label.textAlignment = .right
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        return label
+    }()
+    
+    let forksLabel: UILabel = {
+        let label = UILabel()
+        label.text = "forksLabel"
+        label.tintColor = .secondaryLabel
+        label.textAlignment = .right
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        return label
+    }()
+    
+    let issuesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "issuesLabel"
+        label.tintColor = .secondaryLabel
+        label.textAlignment = .right
+        label.font = .systemFont(ofSize: 20, weight: .regular)
+        return label
+    }()
+    
+    //MARK: - viewDidLoad
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        
+        addSubviews()
+    }
+    
+    private func addSubviews() {
+        view.addSubview(imageView)
+        view.addSubview(titleLabel)
+        view.addSubview(languageLabel)
+        view.addSubview(starsLabel)
+        view.addSubview(watchersLabel)
+        view.addSubview(forksLabel)
+        view.addSubview(issuesLabel)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        
+        let widthSize = (view.frame.size.width)/2.0
+        imageView.frame = CGRect(x: widthSize-widthSize/2.0,
+                                 y: view.safeAreaInsets.top+100,
+                                 width: widthSize,
+                                 height: widthSize)
+        let constraint1 = NSLayoutConstraint(item: imageView,
+                                             attribute: .centerX,
+                                             relatedBy: .equal,
+                                             toItem: view,
+                                             attribute: .centerX,
+                                             multiplier: 1.0,
+                                             constant: 0.0)
+        view.addConstraint(constraint1)
+        
+        titleLabel.frame = CGRect(x: widthSize-widthSize/2.0,
+                                  y: view.safeAreaInsets.top+widthSize+130,
+                                  width: widthSize,
+                                  height: 30)
+        
+        languageLabel.frame = CGRect(x: 20,
+                                     y: view.safeAreaInsets.top+widthSize+200,
+                                     width: widthSize,
+                                     height: 30)
+        
+        starsLabel.frame = CGRect(x: widthSize,
+                                  y: view.safeAreaInsets.top+widthSize+200,
+                                  width: widthSize-10,
+                                  height: 30)
+        
+        watchersLabel.frame = CGRect(x: widthSize,
+                                  y: view.safeAreaInsets.top+widthSize+250,
+                                  width: widthSize-10,
+                                  height: 30)
+        
+        forksLabel.frame = CGRect(x: widthSize,
+                                  y: view.safeAreaInsets.top+widthSize+300,
+                                  width: widthSize-10,
+                                  height: 30)
+        
+        issuesLabel.frame = CGRect(x: widthSize,
+                                  y: view.safeAreaInsets.top+widthSize+350,
+                                  width: widthSize-10,
+                                  height: 30)
+    }
+    
+   
+    
+
+
+}
---
+++ YumemiCodingTest/Views/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  YumemiCodingTest
+//
+//  Created by system on 2021/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var data: [[String: Any]]=[]
+    
+    var task: URLSessionTask?
+    var word: String!
+    var url: String!
+    
+    private let searchBar: UISearchBar = {
+       let searchBar = UISearchBar()
+        searchBar.backgroundColor = .secondarySystemBackground
+        return searchBar
+    }()
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        
+        return tableView
+    }()
+
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(searchBar)
+        view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        searchBar.placeholder = "Search"
+        searchBar.delegate = self
+        
+        navigationController?.navigationBar.topItem?.titleView = searchBar
+    }
+    
+    override func viewDidLayoutSubviews() {
+        tableView.frame = view.bounds
+        
+        
+    }
+
+    
+}
+
+//MARK: - searchBar
+
+extension ViewController: UISearchBarDelegate {
+    
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        
+        return true
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        task?.cancel()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        word = searchBar.text
+        
+        if word.count != 0 {
+            url = "https://api.github.com/search/repositories?q=\(word!)"
+            task = URLSession.shared.dataTask(with: URL(string: url)!) { (data, res, err) in
+                if let obj = try! JSONSerialization.jsonObject(with: data!) as? [String: Any] {
+                    if let items = obj["items"] as? [[String: Any]] {
+                    self.data = items
+                        DispatchQueue.main.async {
+                            self.tableView.reloadData()
+                        }
+                    }
+                }
+            }
+        // これ呼ばなきゃリストが更新されません
+        task?.resume()
+        }
+    }
+}
+
+//MARK: - tableView
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let vc = ResultViewViewController()
+        vc.navigationItem.largeTitleDisplayMode = .never
+        vc.navigationItem.title = "Result"
+        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+
